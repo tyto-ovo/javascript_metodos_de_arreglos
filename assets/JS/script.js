2295efa3c1c62fb33a7f22c4ca850e4c24dd85f5
tyto-ovo <sjollyparker@gmail.com>
2024-08-25 20:13:54 -0400

    estructura lista html

---
+++ assets/JS/script.js
@@ -0,0 +1,5 @@
+const tareas = [
+    {id: 16, descripcion: "Hacer Mercado", completado: false},
+    {id: 60, descripcion: "Estudiar para la prueba", completado: false},
+    {id: 24, descripcion: "Sacar a pasear a Tobby", completado: false},
+]
